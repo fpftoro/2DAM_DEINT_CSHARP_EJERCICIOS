22e540b442f3274db74b98171e4145ea8c06ddc6
Fernando Toro Rueda <ftorrue457@cpifpalanturing.es>
2025-10-06 18:25:04 +0200

    haciendo los ejemplos del pdf de la asignatura

---
+++ UD2_2/ejemplos_pdf/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+delegate int Operacion(int x,int y);
+
+class Program
+{
+    static int Sumar(int a, int b) => a + b;
+    static int Restar(int a, int b) => a - b;
+
+
+    static void Main()
+    {
+
+        Operacion op = Sumar;
+        Console.WriteLine("Suma: " + op(5, 3));
+
+        op = Restar;
+        Console.WriteLine("resta: " + op(5, 3));
+    }
+}
