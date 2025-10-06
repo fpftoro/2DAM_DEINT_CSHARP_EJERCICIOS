87702f87a74a49a088c51f961c7782bd8db58090
Fernando Toro Rueda <ftorrue457@g.educaand.es>
2025-10-06 18:57:05 +0200

    ejemplo de callback

---
+++ UD2_2/ejemplos_pdf/callback/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Data;
+
+
+
+delegate int OperacionCallback(int x, int y);
+
+
+class Program
+{
+    static int Sumar(int a, int b) => a + b;
+
+    static int EjecutarOperacion(OperacionCallback op, int x, int y)
+    { return op(x, y); }
+
+    static void Main()
+    {
+        Console.WriteLine("Suma: "+EjecutarOperacion(Sumar,5,3));
+    }
+
+}
