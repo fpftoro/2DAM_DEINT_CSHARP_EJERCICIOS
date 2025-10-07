59bcf48ca72e510545a2e4cfb720ce7dc21d507d
Fernando Toro <145271414+fpftoro@users.noreply.github.com>
2025-10-07 13:31:08 +0200

    ejemplos del pdf Update Program.cs con comentario

--- UD2_2/ejemplos_pdf/delegados01/Program.cs
+++ UD2_2/ejemplos_pdf/delegados01/Program.cs
@@ -5,6 +5,18 @@ delegate int Operacion(int x,int y);
 
 class Program
 {
+
+    // son en realidad métodos de la clase Program
+    // C# es puro objeto y no existen funciones definidas independientes
+    // como en C,Python o Javascript.
+    // estas funciones están definidas con el método simplificado
+    // la definición no simplificada sería
+    //  static int Sumar(int a, int b) {
+    //   return a+b;
+    // }
+    // la flecha no indica que sea una función lambda
+    // en realidad es una simplificación.
+
     static int Sumar(int a, int b) => a + b;
     static int Restar(int a, int b) => a - b;
 
